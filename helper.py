1c633b6c245f7423b770587de9942e9ada8927b7
Jonathan Benavides Vallejo <maxjbv@gmail.com>
2021-01-02 19:00:28 +0100

    Adding one hot encoded function

--- helper.py
+++ helper.py
@@ -9,3 +9,46 @@ def assets_directory():
 
 def store_dataframe_to_csv(df, filename, columns):
     df.to_csv(filename, columns = columns)
+
+def labels_to_character_map(labels):
+	character_set = set()
+	max_word_size = float('-inf')
+
+	for label in labels:
+		if len(label) > max_word_size:
+			max_word_size = len(label)
+
+		for c in label:
+			character_set.add(c)
+
+	# not starting at zero for usit it as padding
+	count = 1
+
+	character_map = {}
+	for c in character_set:
+		character_map[c] = count
+		count += 1
+
+	return character_map, max_word_size
+
+
+def label_to_one_hot(label, max_word_size, character_map):
+	
+	one_hot = [character_map[c] for c in label]
+
+	if len(one_hot) < max_word_size:
+		padding = [0] * (max_word_size - len(one_hot))
+		one_hot += padding
+
+	return one_hot
+
+
+def words_to_one_hot(labels):
+	character_map, max_word_size = labels_to_character_map(labels)
+
+	word_to_one_hot = {}
+
+	for label in labels:
+		word_to_one_hot[label] = label_to_one_hot(label, max_word_size, character_map)
+
+	return word_to_one_hot
